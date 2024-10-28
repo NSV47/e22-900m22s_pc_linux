19b69835cb1f1f2005f9a3026c2f03b636f91262
Sergey Novikov <nsv47nsv36@yandex.ru>
2024-10-28 22:12:51 +0300

    без GIT'a и жизнь не та

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,133 @@
-#include <Arduino.h>
+/*
+   RadioLib SX126x Receive with Interrupts Example
 
-#define E22_33
+   This example listens for LoRa transmissions and tries to
+   receive them. Once a packet is received, an interrupt is
+   triggered. To successfully receive data, the following
+   settings have to be the same on both transmitter
+   and receiver:
+    - carrier frequency
+    - bandwidth
+    - spreading factor
+    - coding rate
+    - sync word
 
-#include "LoRa_E22.h"
+   Other modules from SX126x family can also be used.
 
-// With FIXED RECEIVER configuration
-// #define DESTINATION_ADDL 2
-#define DESTINATION_ADDL 3
+   For default module settings, see the wiki page
+   https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx126x---lora-modem
 
-LoRa_E22 e22ttl(18, 17, &Serial1, 8, 46, 3, UART_BPS_RATE_9600, SERIAL_8N1); //  esp32 RX <-- e22 TX, esp32 TX --> e22 RX AUX M0 M1 // UART_BPS_RATE_9600
+   For full API reference, see the GitHub Pages
+   https://jgromes.github.io/RadioLib/
+*/
 
-// put function declarations here:
-void printParameters(struct Configuration configuration);
-void printModuleInformation(struct ModuleInformation moduleInformation);
+// include the library
+#include <SPI.h>
+#include <RadioLib.h>
+
+#define SS   10
+#define MOSI 11
+#define MISO 12
+#define SCK  13
+
 void somethingAvailableLORA();
-void initEbyteE22();
+void sendMessage(String&);
+
+// SX1262 has the following connections:
+// NSS pin:   10
+// DIO1 pin:  2
+// NRST pin:  3
+// BUSY pin:  9
+SX1262 radio = new Module(10, 2, 3, 9);
+
+// or using RadioShield
+// https://github.com/jgromes/RadioShield
+//SX1262 radio = RadioShield.ModuleA;
+
+// or using CubeCell
+//SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
+
+// flag to indicate that a packet was received
+volatile bool receivedFlag = false;
+
+// flag to indicate that a packet was sent
+volatile bool transmittedFlag = false;
+
+// this function is called when a complete packet
+// is received by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+#if defined(ESP8266) || defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+void setFlagRec(void) {
+  // we got a packet, set the flag
+  receivedFlag = true;
+}
+
+void setFlag(void) {
+  // we sent a packet, set the flag
+  transmittedFlag = true;
+}
 
 bool stringComplete = false;  // whether the string is complete
 String inputString = "";         // a String to hold incoming data
 
+// save transmission state between loops
+int transmissionState = RADIOLIB_ERR_NONE;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  initEbyteE22();
-  //------------------------------------------
-  Serial1.end();
-  Serial1.begin(115200, SERIAL_8N1, 18, 17);
+  SPI.begin(SCK, MISO, MOSI, SS);
+
+  // initialize SX1262 with default settings
+  Serial.print(F("[SX1262] Initializing ... "));
+  int state = radio.begin();
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
+
+  // set carrier frequency to 433.5 MHz
+  if (radio.setFrequency(868) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+    Serial.println(F("Selected frequency is invalid for this module!"));
+    while (true) { delay(10); }
+  }
+
+  // set the function that will be called
+  // when packet transmission is finished
+  radio.setPacketSentAction(setFlag);
+
+  // set the function that will be called
+  // when new packet is received
+  radio.setPacketReceivedAction(setFlagRec);
+
+  // start listening for LoRa packets
+  Serial.print(F("[SX1262] Starting to listen ... "));
+  state = radio.startReceive();
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
+
+  // if needed, 'listen' mode can be disabled by calling
+  // any of the following methods:
+  //
+  // radio.standby()
+  // radio.sleep()
+  // radio.transmit();
+  // radio.receive();
+  // radio.scanChannel();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   somethingAvailableLORA();
 
   while(Serial.available()>0){
@@ -41,131 +139,110 @@ void loop() {
   if(stringComplete){
     stringComplete = false;
     // SerialPort.print(inputString);
-    ResponseStatus rs = e22ttl.sendFixedMessage(0, DESTINATION_ADDL, 23, inputString); // 20
+    // ResponseStatus rs = e22ttl.sendFixedMessage(0, DESTINATION_ADDL, 23, inputString); // 20
+    sendMessage(inputString);
     inputString = "";
   }
-  
+
+  if(transmittedFlag) {
+    // reset flag
+    transmittedFlag = false;
+
+    if (transmissionState == RADIOLIB_ERR_NONE) {
+      // packet was successfully sent
+      Serial.println(F("transmission finished!"));
+
+      // NOTE: when using interrupt-driven transmit method,
+      //       it is not possible to automatically measure
+      //       transmission data rate using getDataRate()
+
+    } else {
+      Serial.print(F("failed, code "));
+      Serial.println(transmissionState);
+
+    }
+
+    // clean up after transmission is finished
+    // this will ensure transmitter is disabled,
+    // RF switch is powered down etc.
+    radio.finishTransmit();
+  }
+
 }
 
 void somethingAvailableLORA(){
-    // If something available
-    if (e22ttl.available()>1) {
-        // read the String message
-    #ifdef ENABLE_RSSI
-        ResponseContainer rc = e22ttl.receiveMessageRSSI();
-    #else
-        ResponseContainer rc = e22ttl.receiveMessage();
-    #endif
-        // Is something goes wrong print error
-        if (rc.status.code!=1){
-            // Serial.println(rc.status.getResponseDescription());
-        }else{
-            // Print the data received
-            // Serial.println(rc.status.getResponseDescription());
-            
-            Serial.print(rc.data);
-            
-    #ifdef ENABLE_RSSI
-            Serial.print(F("RSSI: ")); Serial.println(rc.rssi, DEC);
-    #endif
-        
-        }
+  // check if the flag is set
+  if(receivedFlag) {
+    // reset flag
+    receivedFlag = false;
+
+    // you can read received data as an Arduino String
+    String str;
+    int state = radio.readData(str);
+
+    // you can also read received data as byte array
+    /*
+      byte byteArr[8];
+      int numBytes = radio.getPacketLength();
+      int state = radio.readData(byteArr, numBytes);
+    */
+
+    if (state == RADIOLIB_ERR_NONE) {
+      // packet was successfully received
+      Serial.println(F("[SX1262] Received packet!"));
+
+      // print data of the packet
+      Serial.print(F("[SX1262] Data:\t\t"));
+      Serial.println(str);
+
+      // print RSSI (Received Signal Strength Indicator)
+      Serial.print(F("[SX1262] RSSI:\t\t"));
+      Serial.print(radio.getRSSI());
+      Serial.println(F(" dBm"));
+
+      // print SNR (Signal-to-Noise Ratio)
+      Serial.print(F("[SX1262] SNR:\t\t"));
+      Serial.print(radio.getSNR());
+      Serial.println(F(" dB"));
+
+      // print frequency error
+      Serial.print(F("[SX1262] Frequency error:\t"));
+      Serial.print(radio.getFrequencyError());
+      Serial.println(F(" Hz"));
+
+    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+      // packet was received, but is malformed
+      Serial.println(F("CRC error!"));
+
+    } else {
+      // some other error occurred
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+
     }
+  }
 }
 
-void initEbyteE22(){
-    e22ttl.begin();
+void sendMessage(String &str){
+  // check if the previous transmission finished
+  
 
-  	ResponseStructContainer c;
+    // wait a second before transmitting again
+    // delay(1000);
 
-  	c = e22ttl.getConfiguration();
+    // send another one
+    // Serial.print(F("[SX1262] Sending another packet ... "));
 
-    // It's important get configuration pointer before all other operation
-    Configuration configuration = *(Configuration*) c.data;
-    Serial.println(c.status.getResponseDescription());
-    Serial.println(c.status.code);
+    // you can transmit C-string or Arduino string up to
+    // 256 characters long
+    // String str = "Hello World! #" + String(count++);
+    transmissionState = radio.startTransmit(str);
 
-    printParameters(configuration);
-
-    //---------------------------------------------------------------------
-    configuration.ADDL = 0x02;
-    // configuration.ADDL = 0x03;
-    configuration.ADDH = 0x00;
-    configuration.NETID = 0x00;
-
-    configuration.CHAN = 23;
-
-    configuration.SPED.uartBaudRate = UART_BPS_115200; // UART_BPS_115200 // UART_BPS_9600
-    configuration.SPED.airDataRate = AIR_DATA_RATE_101_192; // AIR_DATA_RATE_001_12  // AIR_DATA_RATE_011_48 // AIR_DATA_RATE_100_96 // AIR_DATA_RATE_101_192 // AIR_DATA_RATE_110_384 // AIR_DATA_RATE_111_625 
-    configuration.SPED.uartParity = MODE_00_8N1;
-
-    configuration.OPTION.subPacketSetting = SPS_240_00;
-    configuration.OPTION.RSSIAmbientNoise = RSSI_AMBIENT_NOISE_DISABLED;
-    configuration.OPTION.transmissionPower = POWER_24; //POWER_30 // POWER_21
-
-    configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED;                // ENABLE_RSSI
-    configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION;
-    configuration.TRANSMISSION_MODE.enableRepeater = REPEATER_DISABLED;
-    configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED;
-    configuration.TRANSMISSION_MODE.WORTransceiverControl = WOR_TRANSMITTER;
-    configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011;
-
-    //ResponseStatus rs = e22ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_LOSE);
-    e22ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_LOSE);
-
-    printParameters(configuration);
-    //---------------------------------------------------------------------
-
-    ResponseStructContainer cMi;
-    cMi = e22ttl.getModuleInformation();
-    // It's important get information pointer before all other operation
-    ModuleInformation mi = *(ModuleInformation*)cMi.data;
-
-    Serial.println(cMi.status.getResponseDescription());
-    Serial.println(cMi.status.code);
-
-    c.close();
-    cMi.close();
-
-    printModuleInformation(mi);
-}
-
-void printParameters(struct Configuration configuration) {
-    Serial.println("----------------------------------------");
-
-    Serial.print(F("HEAD : "));  Serial.print(configuration.COMMAND, HEX);Serial.print(" ");Serial.print(configuration.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(configuration.LENGHT, HEX);
-    Serial.println(F(" "));
-    Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, HEX);
-    Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, HEX);
-    Serial.print(F("NetID : "));  Serial.println(configuration.NETID, HEX);
-    Serial.println(F(" "));
-    Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
-    Serial.println(F(" "));
-    Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
-    Serial.print(F("SpeedUARTDatte     : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRateDescription());
-    Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRateDescription());
-    Serial.println(F(" "));
-    Serial.print(F("OptionSubPacketSett: "));  Serial.print(configuration.OPTION.subPacketSetting, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getSubPacketSetting());
-    Serial.print(F("OptionTranPower    : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
-    Serial.print(F("OptionRSSIAmbientNo: "));  Serial.print(configuration.OPTION.RSSIAmbientNoise, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getRSSIAmbientNoiseEnable());
-    Serial.println(F(" "));
-    Serial.print(F("TransModeWORPeriod : "));  Serial.print(configuration.TRANSMISSION_MODE.WORPeriod, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
-    Serial.print(F("TransModeTransContr: "));  Serial.print(configuration.TRANSMISSION_MODE.WORTransceiverControl, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORTransceiverControlDescription());
-    Serial.print(F("TransModeEnableLBT : "));  Serial.print(configuration.TRANSMISSION_MODE.enableLBT, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
-    Serial.print(F("TransModeEnableRSSI: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRSSI, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
-    Serial.print(F("TransModeEnabRepeat: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRepeater, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRepeaterModeEnableByteDescription());
-    Serial.print(F("TransModeFixedTrans: "));  Serial.print(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
-
-    Serial.println("----------------------------------------");
-}
-
-void printModuleInformation(struct ModuleInformation moduleInformation) {
-    Serial.println("----------------------------------------");
-    Serial.print(F("HEAD: "));  Serial.print(moduleInformation.COMMAND, HEX);Serial.print(" ");Serial.print(moduleInformation.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(moduleInformation.LENGHT, DEC);
-
-    Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, DEC);
-    Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, DEC);
-    Serial.print(F("Features : "));  Serial.println(moduleInformation.features, BIN);
-    Serial.println("----------------------------------------");
-
+    // you can also transmit byte array up to 256 bytes long
+    /*
+      byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+                        0x89, 0xAB, 0xCD, 0xEF};
+      transmissionState = radio.startTransmit(byteArr, 8);
+    */
+  
 }
