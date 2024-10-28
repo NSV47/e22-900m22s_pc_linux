ec52372f61bb46baf7b13a9ed10060252763d1f5
Sergey Novikov <nsv47nsv36@yandex.ru>
2024-10-28 22:39:42 +0300

    воспользовался примером pingPong

--- src/main.cpp
+++ src/main.cpp
@@ -40,41 +40,29 @@ void sendMessage(String&);
 // BUSY pin:  9
 SX1262 radio = new Module(10, 2, 3, 9);
 
-// or using RadioShield
-// https://github.com/jgromes/RadioShield
-//SX1262 radio = RadioShield.ModuleA;
+bool stringComplete = false;  // whether the string is complete
+String inputString = "";         // a String to hold incoming data
 
-// or using CubeCell
-//SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
+// save transmission states between loops
+int transmissionState = RADIOLIB_ERR_NONE;
 
-// flag to indicate that a packet was received
-volatile bool receivedFlag = false;
+// flag to indicate transmission or reception state
+bool transmitFlag = false;
 
-// flag to indicate that a packet was sent
-volatile bool transmittedFlag = false;
+// flag to indicate that a packet was sent or received
+volatile bool operationDone = false;
 
 // this function is called when a complete packet
-// is received by the module
+// is transmitted or received by the module
 // IMPORTANT: this function MUST be 'void' type
 //            and MUST NOT have any arguments!
 #if defined(ESP8266) || defined(ESP32)
   ICACHE_RAM_ATTR
 #endif
-void setFlagRec(void) {
-  // we got a packet, set the flag
-  receivedFlag = true;
-}
-
 void setFlag(void) {
-  // we sent a packet, set the flag
-  transmittedFlag = true;
+  // we sent or received a packet, set the flag
+  operationDone = true;
 }
-
-bool stringComplete = false;  // whether the string is complete
-String inputString = "";         // a String to hold incoming data
-
-// save transmission state between loops
-int transmissionState = RADIOLIB_ERR_NONE;
 
 void setup() {
   Serial.begin(115200);
@@ -98,32 +86,26 @@ void setup() {
   }
 
   // set the function that will be called
-  // when packet transmission is finished
-  radio.setPacketSentAction(setFlag);
-
-  // set the function that will be called
   // when new packet is received
-  radio.setPacketReceivedAction(setFlagRec);
+  radio.setDio1Action(setFlag);
 
-  // start listening for LoRa packets
-  Serial.print(F("[SX1262] Starting to listen ... "));
-  state = radio.startReceive();
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
-  } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true) { delay(10); }
-  }
-
-  // if needed, 'listen' mode can be disabled by calling
-  // any of the following methods:
-  //
-  // radio.standby()
-  // radio.sleep()
-  // radio.transmit();
-  // radio.receive();
-  // radio.scanChannel();
+  #if defined(INITIATING_NODE)
+    // send the first packet on this node
+    Serial.print(F("[SX1262] Sending first packet ... "));
+    transmissionState = radio.startTransmit("Hello World!");
+    transmitFlag = true;
+  #else
+    // start listening for LoRa packets on this node
+    Serial.print(F("[SX1262] Starting to listen ... "));
+    state = radio.startReceive();
+    if (state == RADIOLIB_ERR_NONE) {
+      Serial.println(F("success!"));
+    } else {
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+      while (true) { delay(10); }
+    }
+  #endif
 }
 
 void loop() {
@@ -144,105 +126,71 @@ void loop() {
     inputString = "";
   }
 
-  if(transmittedFlag) {
+  // check if the previous operation finished
+  if(operationDone) {
     // reset flag
-    transmittedFlag = false;
+    operationDone = false;
 
-    if (transmissionState == RADIOLIB_ERR_NONE) {
-      // packet was successfully sent
-      Serial.println(F("transmission finished!"));
+    if(transmitFlag) {
+      // the previous operation was transmission, listen for response
+      // print the result
+      if (transmissionState == RADIOLIB_ERR_NONE) {
+        // packet was successfully sent
+        Serial.println(F("transmission finished!"));
 
-      // NOTE: when using interrupt-driven transmit method,
-      //       it is not possible to automatically measure
-      //       transmission data rate using getDataRate()
+      } else {
+        Serial.print(F("failed, code "));
+        Serial.println(transmissionState);
+
+      }
+
+      // listen for response
+      radio.startReceive();
+      transmitFlag = false;
 
     } else {
-      Serial.print(F("failed, code "));
-      Serial.println(transmissionState);
+      // the previous operation was reception
+      // print data and send another packet
+      String str;
+      int state = radio.readData(str);
+
+      if (state == RADIOLIB_ERR_NONE) {
+        // packet was successfully received
+        Serial.println(F("[SX1262] Received packet!"));
+
+        // print data of the packet
+        Serial.print(F("[SX1262] Data:\t\t"));
+        Serial.println(str);
+
+        // print RSSI (Received Signal Strength Indicator)
+        Serial.print(F("[SX1262] RSSI:\t\t"));
+        Serial.print(radio.getRSSI());
+        Serial.println(F(" dBm"));
+
+        // print SNR (Signal-to-Noise Ratio)
+        Serial.print(F("[SX1262] SNR:\t\t"));
+        Serial.print(radio.getSNR());
+        Serial.println(F(" dB"));
+
+      }
+
+      // wait a second before transmitting again
+      // delay(1000);
 
     }
-
-    // clean up after transmission is finished
-    // this will ensure transmitter is disabled,
-    // RF switch is powered down etc.
-    radio.finishTransmit();
+  
   }
 
 }
 
 void somethingAvailableLORA(){
-  // check if the flag is set
-  if(receivedFlag) {
-    // reset flag
-    receivedFlag = false;
-
-    // you can read received data as an Arduino String
-    String str;
-    int state = radio.readData(str);
-
-    // you can also read received data as byte array
-    /*
-      byte byteArr[8];
-      int numBytes = radio.getPacketLength();
-      int state = radio.readData(byteArr, numBytes);
-    */
-
-    if (state == RADIOLIB_ERR_NONE) {
-      // packet was successfully received
-      Serial.println(F("[SX1262] Received packet!"));
-
-      // print data of the packet
-      Serial.print(F("[SX1262] Data:\t\t"));
-      Serial.println(str);
-
-      // print RSSI (Received Signal Strength Indicator)
-      Serial.print(F("[SX1262] RSSI:\t\t"));
-      Serial.print(radio.getRSSI());
-      Serial.println(F(" dBm"));
-
-      // print SNR (Signal-to-Noise Ratio)
-      Serial.print(F("[SX1262] SNR:\t\t"));
-      Serial.print(radio.getSNR());
-      Serial.println(F(" dB"));
-
-      // print frequency error
-      Serial.print(F("[SX1262] Frequency error:\t"));
-      Serial.print(radio.getFrequencyError());
-      Serial.println(F(" Hz"));
-
-    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-      // packet was received, but is malformed
-      Serial.println(F("CRC error!"));
-
-    } else {
-      // some other error occurred
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-
-    }
-  }
+  
 }
 
 void sendMessage(String &str){
-  // check if the previous transmission finished
-  
-
-    // wait a second before transmitting again
-    // delay(1000);
-
-    // send another one
-    // Serial.print(F("[SX1262] Sending another packet ... "));
-
-    // you can transmit C-string or Arduino string up to
-    // 256 characters long
-    // String str = "Hello World! #" + String(count++);
-    transmissionState = radio.startTransmit(str);
-
-    // you can also transmit byte array up to 256 bytes long
-    /*
-      byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                        0x89, 0xAB, 0xCD, 0xEF};
-      transmissionState = radio.startTransmit(byteArr, 8);
-    */
-  
+  // send another one
+  Serial.print(F("[SX1262] Sending another packet ... "));
+  // transmissionState = radio.startTransmit("Hello World!");
+  transmissionState = radio.startTransmit(str);
+  transmitFlag = true;
 }
